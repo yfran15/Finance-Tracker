c32d53365e249ee2552b4407722f9bcaef8c66ef
yfran151 <182169575+yfran15@users.noreply.github.com>
2025-05-25 14:36:42 -0400

    Create repositories for the two entities.

--- src/main/java/com/example/demo/entity/Transaction.java
+++ src/main/java/com/example/demo/entity/Transaction.java
@@ -1,51 +1,22 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String description;
+    private String category;
     private double amount;
     private LocalDateTime date;
-
-    // Getter
-    public int getId() {
-        return id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    // Setter
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 
     // Map the transaction to the user
     @ManyToOne
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -11,6 +11,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private int id;
+    @Column (unique = true)
     private String username;
     private String password;
 
